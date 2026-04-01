bd4394f9c3419a3603cbbc3ea415d107d039810a
vandamd <vandamdinh71@gmail.com>
2026-04-02 00:12:14 +0100

    version number

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,15 @@
+import Constants from "expo-constants";
 import ContentContainer from "@/components/ContentContainer";
 import { ToggleSwitch } from "@/components/ToggleSwitch";
 import { useInvertColors } from "@/contexts/InvertColorsContext";
 
 export default function SettingsScreen() {
   const { invertColors, setInvertColors } = useInvertColors();
+  const version = Constants.expoConfig?.version;
+  const headerTitle = version ? `Settings (v${version})` : "Settings";
 
   return (
-    <ContentContainer headerTitle="Settings" hideBackButton>
+    <ContentContainer headerTitle={headerTitle} hideBackButton>
       <ToggleSwitch
         label="Invert Colours"
         onValueChange={setInvertColors}
